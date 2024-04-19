bc008e596a514433c9574cd9df246d0568bc2c24
Sergey Motorny <motorny0919@gmail.com>
2024-04-19 13:26:09 +0300

    start of the project

---
+++ src/test/java/com/motorny/jwtapp/JwtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motorny.jwtapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
