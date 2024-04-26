13d5ab39c13f25b557bee5038c4ebfacc5ae8455
Sergey Motorny <motorny0919@gmail.com>
2024-04-26 15:26:44 +0300

    restoring project changes

--- src/main/java/com/motorny/jwtapp/config/WebSecurityConfig.java
+++ src/main/java/com/motorny/jwtapp/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.motorny.jwtapp.config;
 
+import com.motorny.jwtapp.security.JwtUserDetailsService;
 import com.motorny.jwtapp.security.jwt.AuthEntryPointJwt;
 import com.motorny.jwtapp.security.jwt.JwtTokenFilter;
 import com.motorny.jwtapp.security.jwt.JwtTokenProvider;
@@ -7,10 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -19,6 +21,8 @@ public class WebSecurityConfig {
 
     private JwtTokenProvider jwtTokenProvider;
 
+    private final JwtUserDetailsService jwtUserDetailsService;
+
     @Autowired
     private AuthEntryPointJwt unauthorizedHandler;
 
@@ -26,8 +30,19 @@ public class WebSecurityConfig {
     private static final String LOGIN_ENDPOINT = "/api/v1/auth/login";
 
     @Autowired
-    public WebSecurityConfig(JwtTokenProvider jwtTokenProvider) {
+    public WebSecurityConfig(JwtTokenProvider jwtTokenProvider, JwtUserDetailsService jwtUserDetailsService) {
         this.jwtTokenProvider = jwtTokenProvider;
+        this.jwtUserDetailsService = jwtUserDetailsService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+        authProvider.setUserDetailsService(jwtUserDetailsService);
+        authProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+        return authProvider;
     }
 
     @Bean
@@ -41,18 +56,21 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http)
+    public SecurityFilterChain filterChain(HttpSecurity http, BCryptPasswordEncoder bCryptPasswordEncoder)
             throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(LOGIN_ENDPOINT).permitAll()
-                        .requestMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
-                        .anyRequest().authenticated())
-                .addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(LOGIN_ENDPOINT).permitAll()
+                                .requestMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
+                                .anyRequest().authenticated());
+
+        http.authenticationProvider(authenticationProvider(bCryptPasswordEncoder));
+
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
